548c8d1edf4feebdf5601440b0853af41de030c2
Ghernandez1991 <49377511+Ghernandez1991@users.noreply.github.com>
2019-11-21 12:54:56 -0600

    Delete work folder script.py

--- work folder script.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-#claims list is a list of the claim numbers as a string
-for claim in claims_list:
-    os.system("mkdir " + claim)
-    for j in range (1):
-        os.system("touch " +str(claim)+"/"+"Contentions"+".docx")
